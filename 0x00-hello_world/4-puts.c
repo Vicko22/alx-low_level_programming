b88f5fee398f22ab21e301a8bc0394a038a32b89
daudu victor <dauduvictor111@gmail.com>
2022-09-12 16:07:58 -0700

    4 task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
   * main -Print out string using puts
   *
   * Description: Displaying output using puts 
+  *
   * Using the main function this 
   * code prints "Programming is like building a multilingual puzzle
   * Return:0 
@@ -10,5 +11,4 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
