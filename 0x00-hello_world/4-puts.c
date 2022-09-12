33b3d07521f91b60e20d9ef09f6a1599ecd09636
daudu victor <dauduvictor111@gmail.com>
2022-09-12 16:10:26 -0700

    4 mistake

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,9 @@
   * Using the main function this 
   * code prints "Programming is like building a multilingual puzzle
   * Return:0 
-  */ 
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
