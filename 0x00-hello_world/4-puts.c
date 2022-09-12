73ba8ecf5f44e832f401f943c920f0264f7cce2b
daudu victor <dauduvictor111@gmail.com>
2022-09-12 16:16:10 -0700

    ye

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /**
   * main -Print out string using puts
   *
-  * Description: Displaying output using puts 
-  *
+  * Description: Displaying output using puts
   * Using the main function this 
   * code prints "Programming is like building a multilingual puzzle
-  * Return:0 
+  * Return:0
   */
 int main(void)
 {
