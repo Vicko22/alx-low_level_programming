7c166f654438e3a515a1cde37af833706eea49c0
daudu victor <dauduvictor111@gmail.com>
2022-09-12 14:11:24 -0700

    try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,43 +1,17 @@
-#include <stdio.h>
-
-
-
-
-
-
-
-/**
- *
- *  *  *  main - Entry point
- *
- *   *
- *
- *    *   *
- *
- *     *
- *
- *      *    *  Return: Always 0 (Success)
- *
- *       *
- *
- *        */
-
-
-
-int main(void)
-
-
-
-{
-
-
-
-			puts("\"Programming is like building a multilingual puzzle");
-
-
-
-							return (0);
-
-
-
-}
+#include <stdio.h> 
+  
+ /** 
+  *   * main -Print out string using puts 
+  *    * 
+  *     *Description: Displaying output using puts 
+  *      * 
+  *       * Using the main function this 
+  *        * code prints "Programming is like building a multilingual puzzle 
+  *         * Return:0 
+  *         */ 
+ int main(void) 
+	 { 
+	          puts("\"Programming is like building a multilingual puzzle"); 
+	          return (0); 
+	  }
+<F11>}
