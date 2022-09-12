de1de61b9716c060b522d9cdade6c3c1b0a35a0d
daudu victor <dauduvictor111@gmail.com>
2022-09-12 14:52:26 -0700

    4 and 5 corection

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
 #include <stdio.h> 
   
  /** 
-  *   * main -Print out string using puts 
-  *    * 
-  *     *Description: Displaying output using puts 
-  *      * 
-  *       * Using the main function this 
-  *        * code prints "Programming is like building a multilingual puzzle 
-  *         * Return:0 
-  *         */ 
- int main(void) 
-	 { 
-	          puts("\"Programming is like building a multilingual puzzle"); 
-	          return (0); 
-	  }
-<F11>}
+  * main -Print out string using puts
+  *
+  * Description: Displaying output using puts 
+  * Using the main function this 
+  * code prints "Programming is like building a multilingual puzzle
+  * Return:0 
+  */ 
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
