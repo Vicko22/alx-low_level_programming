cc6cffc3462146ba909afadbade5d33b9484ee70
daudu victor <dauduvictor111@gmail.com>
2022-09-20 05:43:37 -0700

    busy

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry points of
+ * void: description of alphabet
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char w;
+
+	for (w = 'a'; w <= 'z'; w++)
+		{
+			_putchar(w);
+}
+
+	_putchar('\n');
+}
