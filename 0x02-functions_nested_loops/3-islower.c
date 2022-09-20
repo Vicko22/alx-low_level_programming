44afd1b7a8d129efd871f2b55783aebeb93b2497
daudu victor <dauduvictor111@gmail.com>
2022-09-20 12:49:45 -0700

    j

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _islower - checking
+ * c - description
+ * Return:0
+ */
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+
+	{
+		return (1);						
+	
+	}
+
+	else
+	
+	{
+
+		return (0);
+
+	}								}
+
+}
