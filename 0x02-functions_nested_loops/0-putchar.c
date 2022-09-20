c48b119ba966bdfe05957add17dc78729177c938
daudu victor <dauduvictor111@gmail.com>
2022-09-19 20:37:41 -0700

    funcrions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,36 @@
+#include "main"
+/**
+ *
+ *  * main -Entry point
+ *
+ *   *
+ *
+ *    * Return: 0 Always (success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		_putchar('_');
+
+			_putchar('p');
+
+				_putchar('u');
+
+					_putchar('t');
+
+						_putchar('c');
+
+							_putchar('h');
+
+								_putchar('a');
+
+									_putchar('r');
+
+										_putchar('\n');
+
+											return (0);
+
+}
