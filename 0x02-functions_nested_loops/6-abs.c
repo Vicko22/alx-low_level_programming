7bf6f057f2eaaeaab155698e5d26dbda768dcb12
daudu victor <dauduvictor111@gmail.com>
2022-09-21 04:29:03 -0700

    gg

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - compute interger
+ * @n: int type num
+ * Return: 0
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0);
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
