adf60decfd804cfef9bff6c5b0eda215e5599e25
daudu victor <dauduvictor111@gmail.com>
2022-09-21 05:04:31 -0700

    j

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _print_last_digit - last digit print
+ * @n : this is the interger type number
+ * Return: last number value
+ */
+
+int print_last_digit(int n)
+{
+	int r;
+
+	if (n < 0)
+
+
+		r = -1 * (n % 10);
+		_putchar(r + '0');
+		return (r);
+
+
+	else
+		r = n % 10;
+		_putchar(r + '0');
+		return (r);
+
+}
