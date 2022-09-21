892ac885810b8e01eb4525f8ee73f832b6a28fcb
daudu victor <dauduvictor111@gmail.com>
2022-09-21 03:10:06 -0700

    i

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isalpha - alphebet of a-z
+ * @c: tpe int
+ * Return: 1
+ */
+
+int _isalpha(int c)
+
+
+
+{
+
+
+
+			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
+							return (1);
+
+
+
+					else
+
+										return (0);
+
+}
