841edee794d0a21f1c2233d4136b36a8c107c875
daudu victor <dauduvictor111@gmail.com>
2022-09-21 05:09:25 -0700

    u

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,16 +11,19 @@ int print_last_digit(int n)
 	int r;
 
 	if (n < 0)
-
+	{
 
 		r = -1 * (n % 10);
 		_putchar(r + '0');
 		return (r);
-
+	}
 
 	else
+
+	{
 		r = n % 10;
 		_putchar(r + '0');
 		return (r);
 
+	}
 }
