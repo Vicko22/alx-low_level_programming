0c8e13289105243ae7a750658aa117f1ed8be784
daudu victor <dauduvictor111@gmail.com>
2022-09-21 23:42:52 -0700

    check

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test positve or negative int
+ * @void: interger
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	poistive_or_negative(i);
+
+	return (0);
+}
