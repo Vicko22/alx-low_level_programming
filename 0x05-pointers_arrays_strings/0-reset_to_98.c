db54b563aeb43ec3c4c33185a4036851aa81e18b
daudu victor <dauduvictor111@gmail.com>
2022-09-28 06:01:08 -0700

    point

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * @n: interger
+ * Return: Blank.
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers and array plus strings
