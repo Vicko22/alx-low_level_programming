7018ffaa7d2f0b9fdc7b777c94aa7c2b2379aace
daudu victor <dauduvictor111@gmail.com>
2022-10-11 03:44:44 -0700

    new

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - prints the number of arguments passed into it.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
- * Return: 0 
+ * Return: 0
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
