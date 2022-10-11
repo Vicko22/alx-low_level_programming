5fb999ce9438e36a0705ac5275f0fb7655a87da0
daudu victor <dauduvictor111@gmail.com>
2022-10-11 03:31:52 -0700

    new

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Prints all arguments
+ * @argc: Number of command line argument
+ * @argv: Array
+ * Return: 0.
+ */
+
+
+
+int main(int argc, char *argv[])
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < argc; i++)
+
+	printf("%s\n", argv[i]);
+
+
+
+	return (0);
+
+}
